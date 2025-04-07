9e27aa2893f7767414fe30c98eca63787ee0753a
HelaenaCambel <frances.helaena@gmail.com>
2025-04-07 17:53:32 +0800

    remove

--- src/components/Register Details/Buttons.js
+++ src/components/Register Details/Buttons.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import '../../component styles/Register Details/Buttons.css'; 
 
-const Buttons = ({ onSubmit, onAddMushKit }) => {
+const Buttons = ({ onSubmit, onAddMushKit, onRemoveMushKit }) => {
   return (
     <div className="form-buttons">
-      <button
-        type="button"
-        onClick={onAddMushKit}
-        className="add-mushkit-button"
-      >
-        Add MushKit
-      </button>
+      <div className='lef-buttons'>
+        <button
+          type="button"
+          onClick={onAddMushKit}
+          className="add-mushkit-button"
+        >
+          Add MushKit
+        </button>
+
+        <button
+          type="button"
+          onClick={onRemoveMushKit}
+          className="remove-mushkit-button"
+        >
+          Remove MushKit
+        </button>
+      </div>
+      
       <button
         type="submit"
         onClick={onSubmit}
