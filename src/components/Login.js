1208e9cf06c6081485720946f1dbcd87b3889c72
HelaenaCambel <frances.helaena@gmail.com>
2025-04-13 16:52:06 +0800

    login

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState }from "react";
 import NumPad from "../static/NumPad";
 import "../component styles/Login.css"; 
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+
   const handlePinSubmit = (pin) => {
+    console.log("Entered Email:", email);
     console.log("Entered PIN:", pin);
   };
 
@@ -15,8 +18,28 @@ const Login = () => {
 
       <div className="login-container">
         <div className="login-form">
+          <div className="input-container">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
           <h2>Enter PIN</h2>
           <NumPad onSubmit={handlePinSubmit} />
+
+          <div className="forgot-pin">
+            <p>
+              Forgot your PIN?{" "}
+              <a href="/reset">Click here to reset PIN.</a>
+            </p>
+          </div>
+
           <div className="register-text">
             <p>
               Don't have a PIN yet?{" "}
