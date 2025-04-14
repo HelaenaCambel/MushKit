a6945d599946939d68e7a04c4d84e3240a875357
HelaenaCambel <frances.helaena@gmail.com>
2025-04-14 14:54:59 +0800

    new config

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -2,6 +2,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+/*
+//old config
 const firebaseConfig = {
   apiKey: "AIzaSyDTLV_cBbQOCWOzPdGir1T9Dzu7bhctiKQ",
   authDomain: "mushkit-c3234.firebaseapp.com",
@@ -11,6 +13,15 @@ const firebaseConfig = {
   messagingSenderId: "652140384480",
   appId: "1:652140384480:web:eb0b57772f07658c3cc085",
   measurementId: "G-4CWWERTFK7"
+};*/
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDezWcudzYDvUGVXxJfLYxYaqK9WdOuubk",
+  authDomain: "mushkit-df43f.firebaseapp.com",
+  projectId: "mushkit-df43f",
+  storageBucket: "mushkit-df43f.firebasestorage.app",
+  messagingSenderId: "927547852345",
+  appId: "1:927547852345:web:438b9068b8a373ff822fe8"
 };
 
 const app = initializeApp(firebaseConfig);
