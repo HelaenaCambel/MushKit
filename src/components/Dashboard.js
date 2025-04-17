69a27c2edc65bd6da06c73aa2b94cca2abccdeeb
HelaenaCambel <frances.helaena@gmail.com>
2025-04-17 17:43:07 +0800

    formatted  time

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -61,8 +61,16 @@ const Dashboard = () => {
 
   const formatTimestamp = (timestamp) => {
     if (!timestamp) return "Not Available";
+  
     const date = new Date(timestamp);
-    return date.toLocaleString(); 
+  
+    const optionsDate = { year: "numeric", month: "long", day: "numeric" };
+    const optionsTime = { hour: "numeric", minute: "2-digit", hour12: true };
+  
+    const formattedDate = date.toLocaleDateString("en-US", optionsDate);
+    const formattedTime = date.toLocaleTimeString("en-US", optionsTime);
+  
+    return `${formattedDate} @ ${formattedTime}`;
   };  
 
   return (
