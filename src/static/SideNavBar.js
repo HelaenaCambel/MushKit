adef1cfdfe29c3184e6d30926491144afdf4ebba
HelaenaCambel <frances.helaena@gmail.com>
2025-04-20 22:40:03 +0800

    headers - top nav

--- src/static/SideNavBar.js
+++ src/static/SideNavBar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext"; 
 import "./SideNavBar.css";
+import "./SideNavBarMedia.css";
 import { Home, LayoutDashboard, History, User, LogOut } from "lucide-react";
 
 const SideNavBar = ( {onToggle} ) => {
